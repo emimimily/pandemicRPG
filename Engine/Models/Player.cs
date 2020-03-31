b30815f6e3b750d1b3760c7406d249d3d116b5a9
emimimily <52088697+emimimily@users.noreply.github.com>
2020-03-31 15:22:55 -0700

    Add project files.

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Engine.Models
+{
+    class Player
+    {
+        string Name { get; set; }
+        string CharacterClass { get; set; }
+        int Health { get; set; } //hitpoints
+        //int ExperiencePoints { get; set; }
+        //int Level { get; set; }
+        int Money { get; set; } //gold
+    }
+}
